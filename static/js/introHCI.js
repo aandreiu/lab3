cb6d5c8511e2d45810c98c248b16f511b99f5f52
Alexandra Andreiu <aandreiu@ucsd.edu>
2021-01-21 08:09:00 -0800

    updated CSS

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,21 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("You are amazing!");
+			 $(".jumbotron p").toggleClass("active");
 	});
+	$("a.thumbnail").click(projectClick);
 
+function projectClick(e) {
+	 e.preventDefault();
+	 	 var containingProject = $(this).closest(".project");
+	 	 var description = $(containingProject).find(".project-description");
+	 	 if (description.length == 0) {
+			 $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	 	 } else {
+			 $(".project").fadeOut();
+	 }
+ }
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
